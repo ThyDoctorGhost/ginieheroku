244ade81b44660cd253e5a6f31d7a20ca47ce895
Muhammad Hassan Zafar <hassaanzafar007@gmail.com>
2024-12-26 19:16:54 +0500

    Add files via upload

---
+++ product_model.js
@@ -0,0 +1,45 @@
+const DataTypes = require('sequelize');
+
+// Define the Product model
+module.exports = (sequelize) => {
+  const Product = sequelize.define('Product', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vendor_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    sub_category_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+    },
+    image_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    is_available: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  })
+
+  return Product;
+};
